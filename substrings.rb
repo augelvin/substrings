4238d3ce0f01113463e581e3de4de7cfa9ab0e09
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-02-13 18:39:01 -0800

    add method to count occurance of each substring in the dictionary

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,15 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(text, dictionaty)
-    
+def substrings(text, dictionary)
+    text.downcase.concat(' ') # add a space to enable split text if the substring is at the end
+    list = Hash.new
+    dictionary.each do |substring|
+        occurance = text.split(substring).length - 1
+        if occurance > 0
+            list[substring] = occurance
+        end
+    end
+    list
 end
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
